a05d68a33f7d314d0f07b21dca1fcc7b9bc2369c
franco <francojmprogramacion@gmail.com>
2023-10-18 12:42:28 -0300

    refresh session homebanking

--- api/src/routes/homebanking.routes.ts
+++ api/src/routes/homebanking.routes.ts
@@ -7,11 +7,14 @@ import {
 import { loginHBController } from "../modules/homebanking/controller/loginHB.controller";
 import { updateUserdataController } from "../modules/homebanking/controller/updateUserdata.controller";
 import requireHBAccount from "../middleware/requireHBAccount";
+import requireHBSession from "../middleware/requireHBSession";
 
 const router = Router();
 
 router.post("/auth/register", registerHBValidation, registerHBController);
 router.post("/auth/login", loginHBController);
+
+router.post("/auth/session", requireHBSession);
 
 router.patch(
   "/customer/:id",
