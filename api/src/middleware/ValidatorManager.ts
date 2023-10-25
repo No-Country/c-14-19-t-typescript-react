7e1942756393bd469ca8eb0e163dca8cac7b5f46
franrodriguez1993 <francojmprogramacion@gmail.com>
2023-10-25 19:42:48 -0300

    validation fix

--- api/src/middleware/ValidatorManager.ts
+++ api/src/middleware/ValidatorManager.ts
@@ -12,44 +12,52 @@ const validatorManager = (req: Request, res: Response, next: NextFunction) => {
 /**  CREATE CUSTOMER  **/
 
 export const createCustomerValidator = [
-  body("name").trim().escape().isString().isLength({ min: 3, max: 25 }),
-  body("lastname").trim().escape().isString().isLength({ min: 3, max: 25 }),
+  body("name")
+    .trim()
+    .escape()
+    .isString()
+    .isLength({ min: 3, max: 25 })
+    .matches(
+      /^([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$/
+    ),
+  body("lastname")
+    .trim()
+    .escape()
+    .isString()
+    .isLength({ min: 3, max: 25 })
+    .matches(
+      /^([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$/
+    ),
   body("mail").isEmail(),
   body("birthday").trim().isString().isLength({ min: 6, max: 8 }),
-  body("dni").custom((value) => {
-    if (value.toString().length > 8 || value.toString().length < 7) {
-      throw new Error("invalid dni length");
-    }
-    return true;
-  }),
-  body("cellphone").custom((value) => {
-    if (value.toString().length > 12 || value.toString().length < 10) {
-      throw new Error("invalid cellphone length");
-    }
-    return true;
-  }),
+  body("dni").matches(/^\d{7,8}$/),
+  body("cellphone").matches(/^\d{10,12}$/),
   validatorManager,
 ];
 
 /**  CREATE STAFF  **/
 
 export const createStaffValidator = [
-  body("name").trim().escape().isString().isLength({ min: 3, max: 25 }),
-  body("lastname").trim().escape().isString().isLength({ min: 3, max: 25 }),
+  body("name")
+    .trim()
+    .escape()
+    .isString()
+    .isLength({ min: 3, max: 25 })
+    .matches(
+      /^([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$/
+    ),
+  body("lastname")
+    .trim()
+    .escape()
+    .isString()
+    .isLength({ min: 3, max: 25 })
+    .matches(
+      /^([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$/
+    ),
   body("mail").isEmail(),
   body("birthday").trim().isString().isLength({ min: 6, max: 8 }),
-  body("dni").custom((value) => {
-    if (value.toString().length > 8 || value.toString().length < 7) {
-      throw new Error("invalid dni length");
-    }
-    return true;
-  }),
-  body("cellphone").custom((value) => {
-    if (value.toString().length > 12 || value.toString().length < 10) {
-      throw new Error("invalid cellphone length");
-    }
-    return true;
-  }),
+  body("dni").matches(/^\d{7,8}$/),
+  body("cellphone").matches(/^\d{10,12}$/),
   body("username").trim().escape().isString().isLength({ min: 6, max: 25 }),
   body("password").trim().escape().isString().isLength({ min: 6, max: 25 }),
   body("department").trim().escape().isString().isIn(["attention", "hhrr"]),
@@ -69,12 +77,7 @@ export const loginValidator = [
 export const updateValidation = [
   body("mail").isEmail().optional(),
   body("cellphone")
-    .custom((value) => {
-      if (value.toString().length > 12 || value.toString().length < 10) {
-        throw new Error("invalid cellphone length");
-      }
-      return true;
-    })
+    .matches(/^\d{10,12}$/)
     .optional(),
   validatorManager,
 ];
