51ea8610e78e70d13645828b0d16c618af898bc0
Lucio Gastellu Arrieta <104010048+lucio1907@users.noreply.github.com>
2023-10-26 16:01:57 -0300

    release: new form validations

--- client/src/components/staff/register/FormRegisterStaff.tsx
+++ client/src/components/staff/register/FormRegisterStaff.tsx
@@ -28,7 +28,6 @@ const INITIAL_VALUES = {
   password: "",
   department: "",
 };
-const REGEXP = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 const FormRegisterStaff = (): React.ReactElement => {
   const {
