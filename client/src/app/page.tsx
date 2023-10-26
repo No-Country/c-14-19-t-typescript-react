7c89d5b7abc3e7a9d2493f68b9ec2112e681acc8
franrodriguez1993 <francojmprogramacion@gmail.com>
2023-10-26 12:57:10 -0300

    footer

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
-import Navbar from "@/components/Navbar";
+import NavbarHome from "@/components/NavbarHome";
 import React from "react";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "EasyBank - Home",
@@ -9,7 +10,7 @@ export const metadata: Metadata = {
 const App = (): React.ReactElement => {
   return (
     <div>
-      <Navbar />
+      <NavbarHome />
 
       <div className="w-full h-[100vh]">
         <div className="w-full h-full absolute mobile:text-3x1 tablet:text-5xl desktop:text-6xl text-4xl font-extrabold flex flex-col items-start justify-center eb-background">
@@ -27,7 +28,7 @@ const App = (): React.ReactElement => {
         />
       </div>
 
-      <div>holi</div>
+      <Footer />
     </div>
   );
 };
---
+++ client/src/components/NavbarHome.tsx
@@ -0,0 +1,71 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { nunito } from "@/fonts/fonts";
+import LogoutButton from "./buttons/LogoutButton";
+import { useGlobalContext } from "@/hooks/useContext";
+import Image from "next/image";
+import "../app/custom.css";
+
+const NavbarHome = (): React.ReactElement => {
+  const { isAuthorized, setIsAuthorized } = useGlobalContext();
+  const [route, setRoute] = useState<string>("");
+
+  useEffect(() => {
+    const getSession = sessionStorage.getItem("jwtSession");
+    const getDepartment = sessionStorage.getItem("zxcvbn");
+
+    // Comprobar si el usuario está autenticado
+    const getAuthStaff = sessionStorage.getItem("authorized");
+    const getAuthCustomer = sessionStorage.getItem("isCustomer");
+    if (getAuthStaff) setIsAuthorized(!!getAuthStaff);
+    if (getAuthCustomer) setIsAuthorized(!!getAuthCustomer);
+
+    // Obtener sesion y checkear departamento
+    if (getSession) {
+      if (getDepartment === "h") return setRoute("/hhrr/hhrrpanel");
+      if (getDepartment === "a") return setRoute("/staff/staffpanel");
+    }
+  }, []);
+
+  return (
+    <nav
+      className={`flex ${nunito.className} items-center p-3 tablet:h-24 tablet:pr-5 desktop:max-h-32 justify-between no-scrollbar bg-transparent absolute w-full z-50 overflow-y-hidden`}
+    >
+      <div className="p-1 tablet:text-2xl font-extrabold">
+        <Link href="/">
+          <Image
+            src="/logo/easy2wb.png"
+            height={150}
+            width={150}
+            alt="logo-easy"
+          />
+        </Link>
+      </div>
+
+      <div className="flex gap-3 font-medium tablet:text-xl">
+        <ul className="flex gap-5">
+          {!isAuthorized && (
+            <>
+              <Link
+                href="/customer/login"
+                className="font-bold p-1 tablet:p-1 flex items-center justify-center rounded-md desktop:w-[200px] hover:text-white transition-all ease-in duration-200 capitalize eb-button"
+              >
+                Login
+              </Link>
+              <Link
+                href="/customer/register"
+                className="font-bold p-1 tablet:p-1 flex items-center justify-center rounded-md  desktop:w-[200px] hover:text-white transition-all ease-in duration-200 capitalize eb-button"
+              >
+                Register
+              </Link>
+            </>
+          )}
+          {isAuthorized && <LogoutButton />}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavbarHome;
