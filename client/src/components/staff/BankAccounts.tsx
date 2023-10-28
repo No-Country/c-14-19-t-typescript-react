86bdeac0ab74cc980b8d035013b458d1fa7ddb24
Tomas <tomascerdeyraimport@gmail.com>
2023-10-28 19:48:54 -0300

    feat: delete banck account

--- client/src/components/staff/BankAccounts.tsx
+++ client/src/components/staff/BankAccounts.tsx
@@ -1,10 +1,23 @@
+"use client"
 import React, { useEffect, useState } from "react";
 import { account } from "./interfaces/staff.interface";
 import { getAccountList } from "@/utils/accountsRequest";
 import { setTimeout } from "timers";
+import { deleteAccountRequest } from "@/utils/accountsRequest";
 
 const BankAccounts = (id: any): React.ReactElement => {
   const [accountsList, setAccountsList] = useState<Array<account>>([]);
+
+  const deleteAcount = async(id: string) => {
+    const response = confirm("¿Seguro que Quiere eliminar al cliente?");
+    if (response) {
+      const res = await deleteAccountRequest(id);
+      alert(res.msg)
+      
+      
+    } else return;
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       const acounts = await getAccountList(id.id);
@@ -13,7 +26,8 @@ const BankAccounts = (id: any): React.ReactElement => {
       }, 300);
     };
     fetchData();
-  }, [id]);
+  }, [id, deleteAcount]);
+
 
   return (
     <div className="border  border-green-800 w-3/4 max-w-xl  flex  flex-col gap-5 p-3 ">
@@ -28,7 +42,7 @@ const BankAccounts = (id: any): React.ReactElement => {
             <p className=" text-[#41542a]">{account.number_account}</p>
             <p className=" text-green-500 font-bold">${account.money}</p>
             {/* Pasar este div a un componente para hacer el delete de la cuenta */}
-            <button className="font-bold p-1 tablet:p-2 rounded-md bg-[#329556] hover:bg-[#008868] desktop:w-[100px] text-center hover:text-white transition-all ease-in duration-200 capitalize eb-buttonCancel">
+            <button onClick={()=> deleteAcount(account.number_account)} className="font-bold p-1 tablet:p-2 rounded-md bg-[#329556] hover:bg-[#008868] desktop:w-[100px] text-center hover:text-white transition-all ease-in duration-200 capitalize eb-buttonCancel">
               Eliminar
             </button>
           </div>
